0fb3afffa5378aec85e4f81fcdfa33ed9621e2ea
Aurélien Brooke <aurelien.brooke@etu.unistra.fr>
2016-01-08 09:51:16 +0100

    Ajouté les activités Record et Threads

---
+++ app/src/main/java/com/alcatel/mobilevoicemail/ThreadsActivity.java
@@ -0,0 +1,14 @@
+package com.alcatel.mobilevoicemail;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class ThreadsActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_threads);
+    }
+
+}
