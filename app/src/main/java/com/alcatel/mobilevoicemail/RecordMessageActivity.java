9bef168b32af690fa52fb6d5c41859e1cb086ff2
Aurélien Brooke <aurelien.brooke@etu.unistra.fr>
2016-01-08 10:27:36 +0100

    Début du recorder

--- app/src/main/java/com/alcatel/mobilevoicemail/RecordMessageActivity.java
+++ app/src/main/java/com/alcatel/mobilevoicemail/RecordMessageActivity.java
@@ -1,14 +1,38 @@
 package com.alcatel.mobilevoicemail;
 
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class RecordMessageActivity extends ActionBarActivity {
+    private AudioRecord mAudioRecord = null;
+
+    public RecordMessageActivity() {
+        mAudioRecord = new AudioRecord(
+                MediaRecorder.AudioSource.MIC,
+                44100,
+                AudioFormat.CHANNEL_IN_STEREO,
+                AudioFormat.ENCODING_PCM_16BIT,
+                0);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_message);
+
+        final Button recordButton = (Button)findViewById(R.id.record_button);
+        recordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
 }
